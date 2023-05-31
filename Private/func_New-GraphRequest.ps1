d15581b443a9e2c74bdba370a3eabe159e933825
Jeshua Edgar <jeshuaedgar@icloud.com>
2023-05-31 13:51:08 +0200

    new-graphrequest helper

---
+++ Private/func_New-GraphRequest.ps1
@@ -0,0 +1,38 @@
+function New-GraphRequest {
+    param (
+        [parameter (Mandatory = $true)][string]$Endpoint,
+        [parameter (Mandatory = $true)][ValidateSet( "Delete", "Get,", "Patch", "Post", "Put")]$Method,
+        [array]$Body,
+        [switch]$Beta
+    )
+    Test-MSPToolboxConnection
+
+    if ($Beta) {
+        $baseURL = "https://graph.microsoft.com/{0}/" -f "beta"
+    }
+    else {
+        $baseURL = "https://graph.microsoft.com/{0}/" -f "v1.0"
+    }
+
+    #create the splat first
+    $reqSplat = @{
+        Method  = $Method
+        URI     = $baseUrl + $Endpoint
+        Headers = $script:CustomerAuthHeader
+    }
+    if ($Body -is [array]) {
+        $reqSplat.Body += $Body
+    }
+
+    if ($null -eq $script:CustomerAuthHeader) {
+        Write-Warning "You are not using a Partner token, please run 'Connect-MSPToolboxPartner' to connect to a Partner"
+        $reqSplat.Headers = $script:GraphAuthHeader
+    }
+
+    try {
+        Invoke-RestMethod @reqSplat
+    }
+    catch {
+        (Format-ErrorCodes $_).ErrorMessage
+    }
+}
