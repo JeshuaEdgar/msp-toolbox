573fa3f1f6f9f11553af12cff6bef88507ca9154
Jeshua <jeshuaedgar@icloud.com>
2023-07-27 10:40:44 +0200

    refactor of new graph request, added verbose output and pagination support

--- Public/func_New-MSPGraphRequest.ps1
+++ Public/func_New-MSPGraphRequest.ps1
@@ -25,13 +25,39 @@ function New-MSPGraphRequest {
         $reqSplat.Body += $Body
     }
 
+    $reqSplat.GetEnumerator() | ForEach-Object {
+        Write-Verbose "Parameter : $($_.Key)"
+        Write-Verbose "Value     : $($_.Value)"
+    }
+
     if ($null -eq $script:CustomerAuthHeader) {
         Write-Warning "You are not using a Partner token, please run 'Connect-MSPToolboxPartner' to connect to a Partner"
         $reqSplat.Headers = $script:GraphAuthHeader
     }
 
+    # internal function
+    function Add-OutputData ($data) {
+        if ($data.PsObject.Members.Name -contains "value") {
+            $output += $data.value
+        }
+        else {
+            $output += $data
+        }
+    }
+
     try {
-        return (Invoke-RestMethod @reqSplat).value
+        $output = @()
+        $request = Invoke-RestMethod @reqSplat
+        Add-OutputData $request
+        if ($request.'@odata.nextLink') {
+            do {
+                $request = Invoke-RestMethod $request.'@odata.nextLink' -Headers $script:CustomerAuthHeader 
+                Add-OutputData $request
+            } until (
+                (-not $request.'@odata.nextLink')
+            )
+        }
+        return $output
     }
     catch {
         throw (Format-ErrorCodes $_).ErrorMessage
