9d38e4c45b0a944801f272138f6de677bd0b06b4
Jeshua <jeshuaedgar@icloud.com>
2023-08-23 16:35:45 +0200

    added: -Scope optional param
    fixed: token-expiry is in seconds, not minutes
    
    added: permission check

--- Public/func_Connect-MSPPartner.ps1
+++ Public/func_Connect-MSPPartner.ps1
@@ -2,23 +2,28 @@ function Connect-MSPPartner {
     param (
         [CmdletBinding()]
         [parameter(Mandatory = $true)]
-        [string]$TenantID
+        [string]$TenantID,
+        [string]$Scope
     )
     Test-MSPToolboxConnection
+    if (-not $Scope) { $Scope = "https://graph.microsoft.com/.default" }
     $customerTokenSplat = @{
         client_id     = $script:mspToolBoxSession.ApplicationID
         client_secret = $script:mspToolBoxSession.ApplicationSecret
         refresh_token = $script:mspToolBoxSession.Refreshtoken
-        scope         = "https://graph.microsoft.com/.default"
+        scope         = $Scope
         grant_type    = "refresh_token"
     }
     try {
         $customerTokenRequest = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/$($TenantID)/oauth2/v2.0/token" -Body $customerTokenSplat
         $script:CustomerAuthHeader = @{ Authorization = "Bearer $($customerTokenRequest.access_token)"; "Content-Type" = "application/json" }
+        Write-Debug "MSPToolbox | Checking permissions on tenant level..."
+        $permissions = Read-JwtTokenPermissions $customerTokenRequest.access_token
+        $permissions | foreach { Write-Debug $_ }
         # checking one graph request with new token
         $organisationCheck = Invoke-MSPGraphRequest -Method Get -Endpoint "organization"
         $script:mspToolBoxSession.ConnectedTenant = $organisationCheck.displayName
-        $script:mspToolBoxSession.CustomerTokenExpiry = [datetime](Get-Date).AddMinutes($customerTokenRequest.expires_in)
+        $script:mspToolBoxSession.CustomerTokenExpiry = [datetime](Get-Date).AddSeconds($customerTokenRequest.expires_in)
         Write-Output ("Connected to tenant {0}" -f $organisationCheck.displayName)
     }
     catch {
